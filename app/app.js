af53eefa7a801fb4d8c243262e1ef9ea3cc3cb7b
Samuel Vítek <samuel.vitek@email.cz>
2023-11-27 13:33:29 +0200

    revert

--- app/app.js
+++ app/app.js
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+const defaultOptions = {
+    method: METHOD,
+    headers: new Headers({
+        'Content-type': 'application/json',
+        'X-Request-ID': new Date().getTime().toString(),
+    }),
+};
+
+const fetch = require("node-fetch");
+
+var myHeaders = new fetch.Headers();
