c334dc84cae797519348bf5c50aac15aa33329fb
Samuel Vítek <samuel.vitek@email.cz>
2023-11-27 14:33:46 +0200

    change

--- next.config.js
+++ next.config.js
@@ -1,32 +1,17 @@
-// @ts-check
-
 /**
  * @type {import('next').NextConfig}
- **/
+ */
 const nextConfig = {
-    /**
-     * Enable static exports for the App Router.
-     *
-     * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-     */
-    output: "export",
+    output: 'export',
 
-    /**
-     * Set base path. This is usually the slug of your repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "/nextjs-github-pages",
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
 
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-     */
-    images: {
-        unoptimized: true,
-    },
-};
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
 
-module.exports = nextConfig;
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+}
+
+module.exports = nextConfig
